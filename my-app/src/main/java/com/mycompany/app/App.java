aaa5ab13ffe2316335b95d4dcaedaaee5a745b6a
root <tcs.sunil@gmail.com>
2018-02-22 22:35:30 +0000

    final

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "pdte Try Now New Hello World!" );
+        System.out.println( "Try Now New Hello World!" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
